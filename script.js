cfe926d5e4db8f6911fe48eb1a519ff9bc28e38b
Bhupesh kumawat <Bhupeshkumawat100@gmail.com>
2021-04-07 08:11:29 +0530

    work is going on

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+const quoteContainers = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+// using local file NOT online API--------
 function newQuotes() {
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote);
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
 }
 
 newQuotes();
+
