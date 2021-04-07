af5c22291764bd690f92818c480dfd6464b581e1
Bhupesh kumawat <Bhupeshkumawat100@gmail.com>
2021-04-08 03:32:31 +0530

    ALL IS WELL

--- script.js
+++ script.js
@@ -8,9 +8,21 @@ const newQuoteBtn = document.getElementById('new-quote');
 // using local file NOT online API--------
 function newQuotes() {
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    // Check if Author field is blank and replace it with "Unknown"
+    if(quote.author === null) {
+     authorText.textContent = "Unknown";  
+    }else {
+        authorText.textContent = quotw.author;
+    }
+    //  Check Quote length to determine styling
+    if(quote.text.length > 50) {
+        quoteText.classList
+    }
+
     authorText.textContent = quote.author;
     quoteText.textContent = quote.text;
 }
+
 
 newQuotes();
 
